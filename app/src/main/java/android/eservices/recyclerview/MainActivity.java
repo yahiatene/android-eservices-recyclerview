fc21b87896bb52fb4178648b6db0234b30e5e0ec
yahiatene <virus385625>
2019-11-21 17:33:09 +0100

    end tp

--- app/src/main/java/android/eservices/recyclerview/MainActivity.java
+++ app/src/main/java/android/eservices/recyclerview/MainActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements GameActionInterface{
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private Toolbar toolbar;
     private CoordinatorLayout coordinatorLayout;
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupRecyclerView() {
         //TODO Bind recyclerview and set its adapter.
+        recyclerView = (RecyclerView) findViewById(R.id.my_recyclerview);
+
+        layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        // specify an adapter (see also next example)
+        myAdapter = new MyAdapter(DataGenerator.generateData(), this);
+        recyclerView.setAdapter(myAdapter);
+
+        myAdapter.notifyDataSetChanged();
 
         //Use data generator to get data to display.
     }
 
     public void displaySnackBar(String message) {
         //TODO write a method that displays a snackbar in the coordinator layout with the "message" parameter as content.
+
+        CoordinatorLayout coord = findViewById(R.id.coordinator_layout);
+
+        Snackbar.make(coord,message,Snackbar.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onGameInfoClicked(String gameTitle) {
+        displaySnackBar(gameTitle);
+    }
+
+    @Override
+    public void onGameClicked(String gameTitle) {
+        displaySnackBar(gameTitle);
     }
 
     //TODO create callback methods for item click
